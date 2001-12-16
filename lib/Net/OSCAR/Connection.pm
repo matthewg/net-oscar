52fdce41f4b2079d798f050fc705d55e1955198b
Matthew Sachs <matthewg@zevils.com>
2001-12-16 18:58:56 +0000

    Attempt to not crap out when connection reset by peer

--- lib/Net/OSCAR/Connection.pm
+++ lib/Net/OSCAR/Connection.pm
@@ -48,6 +48,9 @@ sub flap_encode($$;$) {
 
 sub flap_put($$;$) {
 	my($self, $msg, $channel) = @_;
+
+	return unless $self->{socket} and $self->{socket}->opened and $self->{socket}->connected and !$self->{socket}->error and !$self->{socket}->eof;
+
 	my $emsg = $self->flap_encode($msg, $channel);
 	syswrite($self->{socket}, $emsg, length($emsg)) or return $self->{session}->crapout($self, "Couldn't write to socket: $!");
 	$self->log_print(OSCAR_DBG_PACKETS, "Put ", hexdump($emsg));
