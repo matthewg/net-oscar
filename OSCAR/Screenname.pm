b209336630b783e96e9a8bf9be9b1d13939c7409
Matthew Sachs <matthewg@zevils.com>
2001-12-19 03:56:05 +0000

    More fixes to try and stop infinite loop of Net::OSCAR::Screenname::{boolify,compare}

--- OSCAR/Screenname.pm
+++ OSCAR/Screenname.pm
@@ -14,10 +14,10 @@ use overload
 	"bool" => "boolify";
 
 sub new($$) {
-	return $_[1] if ref($_[0]);
+	return $_[1] if ref($_[0]) or $_[1]->isa("Net::OSCAR::Screenname");
 	my $class = ref($_[0]) || $_[0] || "Net::OSCAR::Screenname";
 	shift;
-	my $name = shift;
+	my $name = "$_[1]"; # Make doubleplus sure that name isn't one of us
 	my $self = \$name;
 	bless $self, $class;
 	return $self;
