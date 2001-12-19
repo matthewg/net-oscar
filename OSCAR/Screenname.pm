7ca224d512f5bdec1903a4c5b80aec5ba39e8f7a
Matthew Sachs <matthewg@zevils.com>
2001-12-19 04:22:46 +0000

    think-o fix

--- OSCAR/Screenname.pm
+++ OSCAR/Screenname.pm
@@ -17,7 +17,7 @@ sub new($$) {
 	return $_[1] if ref($_[0]) or UNIVERSAL::isa($_[1], "Net::OSCAR::Screenname");
 	my $class = ref($_[0]) || $_[0] || "Net::OSCAR::Screenname";
 	shift;
-	my $name = "$_[1]"; # Make doubleplus sure that name isn't one of us
+	my $name = "$_[0]"; # Make doubleplus sure that name isn't one of us
 	my $self = \$name;
 	bless $self, $class;
 	return $self;
