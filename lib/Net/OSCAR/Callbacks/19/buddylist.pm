38fa126499ca32d91aaeb557e89e1c7b11a866f0
Matthew Sachs <matthewg@zevils.com>
2005-12-28 00:18:48 +0000

    Fix support for very large buddylists

--- lib/Net/OSCAR/Callbacks/19/buddylist.pm
+++ lib/Net/OSCAR/Callbacks/19/buddylist.pm
@@ -7,14 +7,14 @@ sub {
 $session->{blarray} ||= [];
 substr($data{data}, 0, 3) = "";
 substr($data{data}, -4, 4) = "" if $snac->{flags2};
-$session->{blarray}->[$snac->{flags2}] = $data{data};
+push @{$session->{blarray}}, $data{data};
 
 if($snac->{flags2}) {
 	$connection->log_printf(OSCAR_DBG_SIGNON, "Got buddylist segment -- need %d more.", $snac->{flags2});
 } else {
 	delete $session->{gotbl};
 
-	Net::OSCAR::_BLInternal::blparse($session, join("", reverse @{$session->{blarray}}));
+	Net::OSCAR::_BLInternal::blparse($session, join("", @{$session->{blarray}}));
 	delete $session->{blarray};
 	got_buddylist($session, $connection);
 }
